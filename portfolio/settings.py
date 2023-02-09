8e6f939f0bae3966b6c74ef3122e4e047b59d393
angeloabianchi <angeloabianchi@gmail.com>
2023-02-09 18:30:01 +0100

    db created and set

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -75,10 +75,29 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
+    'sqlite3': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+    },
+    'mysql': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': config('MySQL_DB_NAME'),
+        'USER': config('MySQL_DB_USER'),
+        'PASSWORD': config('MySQL_DB_PASSWORD'),
+        'HOST': config('MySQL_DB_HOST'),
+        'PORT': config('MySQL_DB_PORT'),
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+            'charset': config('MySQL_DB_CHAR_ENCODING'),
+        },
     }
 }
 
